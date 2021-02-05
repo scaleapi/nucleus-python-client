592ed7d785b01c13ceaaddd3e8c00619571a6a5f
Jihan Yin <jihan@ip-192-168-50-100.us-west-2.compute.internal>
2021-02-05 13:18:11 -0800

    api working, tested

--- nucleus/slice.py
+++ nucleus/slice.py
@@ -1,4 +1,5 @@
-from .constants import DATASET_ITEM_ID_KEY
+from typing import List, Dict, Any
+from .constants import DATASET_ITEM_ID_KEY, REFERENCE_IDS_KEY
 
 
 class Slice:
@@ -27,25 +28,37 @@ class Slice:
         """
         return self._client.slice_info(self.slice_id, id_type)
 
-    def append(self, payload: dict) -> dict:
+    def append(
+        self, 
+        dataset_item_ids: List[str] = None,
+        reference_ids: List[str] = None
+    ) -> dict:
         """
         Appends to a slice from items already present in a dataset.
         The caller must exclusively use either datasetItemIds or reference_ids
         as a means of identifying items in the dataset.
 
         :param
-        payload:
-        {
-            "dataset_item_ids": List[str],
-            "reference_ids": List[str],
-        }
-        
+        dataset_item_ids: List[str],
+        reference_ids: List[str],
+
         :return:
         {
             "slice_id": str,
         }
         """
-        response = self.client._make_request(
+        if dataset_item_ids and reference_ids:
+            raise Exception(
+                "You cannot both dataset_item_ids and reference_ids"
+            )
+
+        payload: Dict[str, Any] = {}
+        if dataset_item_ids:
+            payload[DATASET_ITEM_IDS_KEY] = dataset_item_ids
+        if reference_ids:
+            payload[REFERENCE_IDS_KEY] = reference_ids
+
+        response = self._client._make_request(
             payload, f"slice/{self.slice_id}/append"
         )
         return response
