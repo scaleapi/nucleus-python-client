fc3fddd64c8dc5d5b9d1a6f8d971f6aea4f3ea10
Russell Kaplan <russell.kaplan@scale.com>
2021-02-07 23:23:52 -0800

    Add more thorough asserts for dataset create/delete test, and add a dataset fixture

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -6,10 +6,24 @@ from nucleus import Dataset
 
 TEST_DATASET_NAME = '[PyTest] Test Dataset'
 
+@pytest.fixture(scope='module')
+def dataset(CLIENT):
+    ds = CLIENT.create_dataset(TEST_DATASET_NAME)
+    yield ds
+
+    CLIENT.delete_dataset(ds.id)
+
+
 def test_dataset_create_and_delete(CLIENT):
     # Creation
     ds = CLIENT.create_dataset(TEST_DATASET_NAME)
     assert isinstance(ds, Dataset)
+    assert ds.name == TEST_DATASET_NAME
+    assert ds.model_runs == []
+    assert ds.slices == []
+    assert ds.size == 0
+    assert ds.items == []
 
     # Deletion
     response = CLIENT.delete_dataset(ds.id)
+    assert response == {}
