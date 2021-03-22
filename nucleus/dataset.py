006abfba3b5578dd6cdc6bc6c846332d224f7d8b
Sasha Harrison <sasha.harrison@scale.com>
2021-03-22 08:56:38 -0700

    nit

--- nucleus/dataset.py
+++ nucleus/dataset.py
@@ -264,4 +264,4 @@ class Dataset:
         return self._client.delete_custom_index(self.id)
 
     def check_index_status(self, job_id: str):
-        return self._client.check_index_status(self, job_id)
+        return self._client.check_index_status(job_id)
