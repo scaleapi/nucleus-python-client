f0a57cfdf94c7e658f1ae5823b989b4d42c5f8e3
Sasha Harrison <sasha.harrison@scale.com>
2021-08-09 15:57:48 -0700

    dummy commit 2

--- nucleus/annotation.py
+++ nucleus/annotation.py
@@ -319,7 +319,6 @@ def check_all_frame_paths_remote(frames: List[str]):
             )
 
 
-# local uploads are currently unsupoorted from segmentation masks
 def check_all_mask_paths_remote(
     annotations: Sequence[Union[Annotation]],
 ):
