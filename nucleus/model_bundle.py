477dbd39a2c6c318a7b58b1402f0c63d2d5c9248
Sean Shi <sean.shi@scale.com>
2021-08-17 16:28:33 -0700

    function stubs for hosted model inference client

---
+++ nucleus/model_bundle.py
@@ -0,0 +1,29 @@
+from .dataset import Dataset
+from typing import Any
+
+class ModelBundle:
+    def __init__(self):
+        # TODO: stub
+        pass
+
+
+class ModelEndpoint:
+    def __init__(self):
+        # TODO: stub
+        pass
+
+    def create_run_job(self, model_name: str, dataset: Dataset):
+        # TODO: stub
+        raise NotImplementedError
+
+
+def add_model_bundle(model_name: str, model: Any, load_predict_fn: Any, reference_id: str):
+    # TODO: stub
+    raise NotImplementedError
+
+
+def create_model_endpoint(endpoint_name: str, model_bundle: ModelBundle, cpus: int, memory: str, gpus: int, gpu_type: str, sync_type: str, min_workers: int, max_workers: int):
+    # TODO: stub
+    # TODO: input validation?
+    # This should make an HTTP request to the Hosted Model Inference server at the "create model endpoint" endpoint
+    raise NotImplementedError
