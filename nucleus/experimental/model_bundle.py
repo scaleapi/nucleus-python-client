407d5ef9d35398a75d63d426005b928df2f8a114
Sean Shi <sean.shi@scale.com>
2021-08-18 17:30:58 -0700

    move to experimental folder

--- nucleus/experimental/model_bundle.py
+++ nucleus/experimental/model_bundle.py
@@ -1,7 +1,8 @@
-from .dataset import Dataset
+from nucleus.dataset import Dataset
 from typing import Any
 import tempfile
 import dill
+import smart_open
 
 
 class ModelBundle:
@@ -25,7 +26,29 @@ class ModelEndpoint:
 # TODO: add these to __init__
 
 def add_model_bundle(model_name: str, model: Any, load_predict_fn: Any, reference_id: str):
+    """
+    Uploads to s3 (for now, will upload to actual service later) a model bundle, i.e. a dictionary
+    {
+        "model": model
+        "load_predict_fn": load_predict_fn
+    }
+    """
     # TODO: stub, types of model and load_predict_fn
+    # For now we do some s3 string manipulation
+    s3_path = f"s3://scale-ml/hosted-model-inference/bundles/{model_name}_{reference_id}.pkl"
+    # this might be an invalid url but this is temporary anyways
+    with smart_open.open(s3_path, "r") as bundle_pkl:
+        bundle = dict(model=model, load_predict_fn=load_predict_fn)
+        dill.dump(bundle, bundle_pkl)
+
+        # TODO upload the file via http request later
+
+    # TODO make request to hosted model inference (hmm how will that work?
+    #  We probably want to abstract out the make_request thing but there's already some work inside this library)
+
+
+
+
 
 
     raise NotImplementedError
