ce1c32ccdc63920f0fa0d08c8f614e2806e0bb42
Drake Wong <drakejwong@gmail.com>
2021-10-20 10:23:11 -0700

    lint

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,19 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Nucleus'
-copyright = '2021, Scale'
-author = 'Scale'
+project = "Nucleus"
+copyright = "2021, Scale"
+author = "Scale"
 
 # The full version, including alpha/beta/rc tags
 from nucleus import __version__
+
 release = "v" + str(__version__)
 
 
@@ -32,12 +34,12 @@ release = "v" + str(__version__)
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -50,12 +52,12 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 autosummary_generate = True
 autosummary_imported_members = True
