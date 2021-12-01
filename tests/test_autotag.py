586c6feb7d54b11dd87d2034027d37ddc8aac414
Ubuntu <diego.ardila@scale.com>
2021-12-01 01:25:54 +0000

    fix broken test by creating slice

--- tests/test_autotag.py
+++ tests/test_autotag.py
@@ -88,8 +88,8 @@ def test_dataset_export_autotag_tagged_items(CLIENT):
 
 
 def test_export_slice_embeddings(CLIENT):
-    test_slice = CLIENT.get_slice("slc_c4s4ts3v7bw00b1hkj0g")
     if running_as_nucleus_pytest_user(CLIENT):
+        test_slice = CLIENT.get_slice("slc_c6kcx5mrzr7g0c9d8cng")
         embeddings = test_slice.export_embeddings()
         assert "embedding_vector" in embeddings[0]
         assert "reference_id" in embeddings[0]
