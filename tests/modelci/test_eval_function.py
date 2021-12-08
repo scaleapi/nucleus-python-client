7fe3a7575c543d887edd623e981771da08d9300c
phil-scale <phil.chen@scale.com>
2021-12-08 12:00:48 -0800

    Remove bad IOU function

--- tests/modelci/test_eval_function.py
+++ tests/modelci/test_eval_function.py
@@ -3,5 +3,5 @@ from nucleus.modelci import EvaluationCriterion
 
 def test_list_eval_functions(CLIENT):
     eval_functions = CLIENT.modelci.eval_functions
-    criteria = eval_functions.iou() > 0.5
+    criteria = eval_functions.bbox_iou() > 0.5
     assert isinstance(criteria, EvaluationCriterion)
