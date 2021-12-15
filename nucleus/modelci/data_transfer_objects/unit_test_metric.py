1309d0ec036e88d60f13e55c6c8e18a116284a21
Drake Wong <drakejwong@gmail.com>
2021-12-14 19:21:35 -0800

    lint

--- nucleus/modelci/data_transfer_objects/unit_test_metric.py
+++ nucleus/modelci/data_transfer_objects/unit_test_metric.py
@@ -4,7 +4,7 @@ from ..constants import ThresholdComparison
 
 
 class AddUnitTestMetric(ImmutableModel):
-    """ Data transfer object to add a unit test."""
+    """Data transfer object to add a unit test."""
 
     unit_test_name: str
     eval_function_id: str
