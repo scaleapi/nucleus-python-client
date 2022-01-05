d4b0fb9784209524310036bbca209fcd084b81da
phil-scale <92065453+phil-scale@users.noreply.github.com>
2022-01-05 14:48:27 -0800

    Update nucleus/metrics/errors.py
    Co-authored-by: Gunnar Atli Thoroddsen <gunnarthoroddsen@gmail.com>

--- nucleus/metrics/errors.py
+++ nucleus/metrics/errors.py
@@ -1,7 +1,7 @@
 class PolygonAnnotationTypeError(Exception):
     def __init__(
         self,
-        message="Annotation was expected to be type BoxAnnotation or PolygonAnnotation.",
+        message="Annotation was expected to be of type 'BoxAnnotation' or 'PolygonAnnotation'.",
     ):
         self.message = message
         super().__init__(self.message)
