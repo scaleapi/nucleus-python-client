5cdb916028ab7eee859a9b1dcbbacdc22115b339
Phil Scale <philscale@DNa80923d.SUNet>
2022-01-05 21:53:29 -0800

    Fix tests

--- tests/metrics/helpers.py
+++ tests/metrics/helpers.py
@@ -1,30 +1,85 @@
 import pytest
 
-from nucleus.annotation import BoxAnnotation, PolygonAnnotation
-from nucleus.metrics.base import Metric, MetricResult
+from nucleus.annotation import BoxAnnotation, PolygonAnnotation, AnnotationList
+from nucleus.prediction import BoxPrediction, PolygonPrediction, PredictionList
 from tests.helpers import TEST_BOX_ANNOTATIONS, TEST_CONVEX_POLYGON_ANNOTATIONS
 
-TEST_BOX_ANNOTATIONS = [
-    BoxAnnotation(**annotation) for annotation in TEST_BOX_ANNOTATIONS
-]
+TEST_BOX_ANNOTATION_LIST = AnnotationList(
+    box_annotations=[
+        BoxAnnotation(**annotation) for annotation in TEST_BOX_ANNOTATIONS
+    ]
+)
+
+TEST_BOX_PREDICTION_LIST = PredictionList(
+    box_predictions=[
+        BoxPrediction(**annotation) for annotation in TEST_BOX_ANNOTATIONS
+    ]
+)
 
 
-TEST_CONVEX_POLYGON_ANNOTATIONS = [
-    PolygonAnnotation.from_json(annotation)
-    for annotation in TEST_CONVEX_POLYGON_ANNOTATIONS
-]
+TEST_CONVEX_POLYGON_ANNOTATION_LIST = AnnotationList(
+    polygon_annotations=[
+        PolygonAnnotation.from_json(annotation)
+        for annotation in TEST_CONVEX_POLYGON_ANNOTATIONS
+    ]
+)
 
+TEST_CONVEX_POLYGON_PREDICTION_LIST = PredictionList(
+    polygon_predictions=[
+        PolygonPrediction.from_json(annotation)
+        for annotation in TEST_CONVEX_POLYGON_ANNOTATIONS
+    ]
+)
+
+
+TEST_ANNOTATION_LIST = AnnotationList(
+    box_annotations=[
+        BoxAnnotation(
+            label="car",
+            x=0,
+            y=0,
+            width=10,
+            height=10,
+            reference_id="image_1",
+            annotation_id="image_1_car_box_1",
+        ),
+        BoxAnnotation(
+            label="car",
+            x=5,
+            y=5,
+            width=3,
+            height=3,
+            reference_id="image_1",
+            annotation_id="image_1_car_box_1",
+        )
+    ]
+)
+
+TEST_PREDICTION_LIST = PredictionList(
+    box_predictions=[
+        BoxPrediction(
+            label="car",
+            x=0,
+            y=0,
+            width=10,
+            height=10,
+            reference_id="image_1",
+            confidence=0.9,
+            annotation_id="image_1_car_box_1",
+        ),
+        BoxPrediction(
+            label="car",
+            x=5,
+            y=5,
+            width=10,
+            height=10,
+            reference_id="image_1",
+            confidence=0.9,
+            annotation_id="image_1_car_box_1",
+        )
+    ]
+)
 
 def assert_metric_eq(actual, expected):
     assert expected.value == pytest.approx(actual.value)
     assert expected.weight == pytest.approx(actual.weight)
-
-
-def perfect_match_test_wrapper(test_data, metric_fn):
-    metric = metric_fn(enforce_label_match=False)
-    result = metric(test_data, test_data)
-    assert_metric_eq(result, MetricResult(1, len(test_data)))
-
-    metric = metric_fn(enforce_label_match=True)
-    result = metric(test_data, test_data)
-    assert_metric_eq(result, MetricResult(1, len(test_data)))
--- tests/metrics/test_polygon_metrics.py
+++ tests/metrics/test_polygon_metrics.py
@@ -1,20 +1,69 @@
+import pytest
+from copy import deepcopy
+
 from nucleus.metrics import PolygonIOU, PolygonPrecision, PolygonRecall
+from nucleus.metrics.base import Metric, MetricResult
 from tests.metrics.helpers import (
-    TEST_BOX_ANNOTATIONS,
-    TEST_CONVEX_POLYGON_ANNOTATIONS,
-    perfect_match_test_wrapper,
+    TEST_ANNOTATION_LIST,
+    TEST_PREDICTION_LIST,
+    TEST_BOX_ANNOTATION_LIST,
+    TEST_BOX_PREDICTION_LIST,
+    TEST_CONVEX_POLYGON_ANNOTATION_LIST,
+    TEST_CONVEX_POLYGON_PREDICTION_LIST,
+    assert_metric_eq,
 )
 
 
-def test_perfect_match_box_metrics():
-    perfect_match_test_wrapper(TEST_BOX_ANNOTATIONS, PolygonIOU)
-    perfect_match_test_wrapper(TEST_BOX_ANNOTATIONS, PolygonPrecision)
-    perfect_match_test_wrapper(TEST_BOX_ANNOTATIONS, PolygonRecall)
+@pytest.mark.parametrize("test_annotations,test_predictions,metric_fn", [
+    (TEST_BOX_ANNOTATION_LIST, TEST_BOX_PREDICTION_LIST, PolygonIOU),
+    (TEST_BOX_ANNOTATION_LIST, TEST_BOX_PREDICTION_LIST, PolygonPrecision),
+    (TEST_BOX_ANNOTATION_LIST, TEST_BOX_PREDICTION_LIST, PolygonRecall),
+    (TEST_CONVEX_POLYGON_ANNOTATION_LIST, TEST_CONVEX_POLYGON_PREDICTION_LIST, PolygonIOU),
+    (TEST_CONVEX_POLYGON_ANNOTATION_LIST, TEST_CONVEX_POLYGON_PREDICTION_LIST, PolygonPrecision),
+    (TEST_CONVEX_POLYGON_ANNOTATION_LIST, TEST_CONVEX_POLYGON_PREDICTION_LIST, PolygonRecall),
+])
+def test_perfect_match_polygon_metrics(test_annotations, test_predictions, metric_fn):
+    # Test metrics on where annotations = predictions perfectly
+    metric = metric_fn(enforce_label_match=False)
+    result = metric(test_annotations, test_predictions)
+    assert_metric_eq(result, MetricResult(1, len(test_annotations)))
+
+    metric = metric_fn(enforce_label_match=True)
+    result = metric(test_annotations, test_predictions)
+    assert_metric_eq(result, MetricResult(1, len(test_annotations)))
 
 
-def test_perfect_match_polygon_metrics():
-    perfect_match_test_wrapper(TEST_CONVEX_POLYGON_ANNOTATIONS, PolygonIOU)
-    perfect_match_test_wrapper(
-        TEST_CONVEX_POLYGON_ANNOTATIONS, PolygonPrecision
-    )
-    perfect_match_test_wrapper(TEST_CONVEX_POLYGON_ANNOTATIONS, PolygonRecall)
+@pytest.mark.parametrize("test_annotations,test_predictions,metric_fn", [
+    (TEST_BOX_ANNOTATION_LIST, TEST_BOX_PREDICTION_LIST, PolygonIOU),
+    (TEST_BOX_ANNOTATION_LIST, TEST_BOX_PREDICTION_LIST, PolygonPrecision),
+    (TEST_BOX_ANNOTATION_LIST, TEST_BOX_PREDICTION_LIST, PolygonRecall),
+    (TEST_CONVEX_POLYGON_ANNOTATION_LIST, TEST_CONVEX_POLYGON_PREDICTION_LIST, PolygonIOU),
+    (TEST_CONVEX_POLYGON_ANNOTATION_LIST, TEST_CONVEX_POLYGON_PREDICTION_LIST, PolygonPrecision),
+    (TEST_CONVEX_POLYGON_ANNOTATION_LIST, TEST_CONVEX_POLYGON_PREDICTION_LIST, PolygonRecall),
+])
+def test_perfect_unmatched_polygon_metrics(test_annotations, test_predictions, metric_fn):
+    # Test metrics on where annotations and predictions do not have matching reference IDs.
+    test_predictions_unmatch = deepcopy(test_predictions)
+    for box in test_predictions_unmatch.box_predictions:
+        box.reference_id += "_bad"
+    for polygon in test_predictions_unmatch.polygon_predictions:
+        polygon.reference_id += "_bad"
+    metric = metric_fn(enforce_label_match=False)
+    result = metric(test_annotations, test_predictions_unmatch)
+    assert_metric_eq(result, MetricResult(0, len(test_annotations)))
+
+    metric = metric_fn(enforce_label_match=True)
+    result = metric(test_annotations, test_predictions_unmatch)
+    assert_metric_eq(result, MetricResult(0, len(test_annotations)))
+
+
+@pytest.mark.parametrize("test_annotations,test_predictions,metric_fn,expected", [
+    (TEST_ANNOTATION_LIST, TEST_PREDICTION_LIST, PolygonIOU, MetricResult(0.545, 2)),
+    (TEST_ANNOTATION_LIST, TEST_PREDICTION_LIST, PolygonPrecision, MetricResult(0.5, 2)),
+    (TEST_ANNOTATION_LIST, TEST_PREDICTION_LIST, PolygonRecall, MetricResult(0.5, 2)),
+])
+def test_simple_2_boxes(test_annotations, test_predictions, metric_fn, expected):
+    # Test metrics on where annotations = predictions perfectly
+    metric = metric_fn()
+    result = metric(test_annotations, test_predictions)
+    assert_metric_eq(result, expected)
