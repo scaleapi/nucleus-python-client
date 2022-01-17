a0d58c2fccba08fd166d9ae78083d2022c274b18
Gunnar Atli Thoroddsen <gunnar.thoroddsen@scale.com>
2022-01-17 13:00:03 +0000

    Add shapely specific import error

--- nucleus/metrics/__init__.py
+++ nucleus/metrics/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 from .base import Metric, MetricResult
 from .polygon_metrics import (
     PolygonIOU,
@@ -5,3 +7,22 @@ from .polygon_metrics import (
     PolygonPrecision,
     PolygonRecall,
 )
+
+try:
+    import shapely
+except ModuleNotFoundError as e:
+    if sys.platform.startswith("darwin"):
+        platform_specific_msg = (
+            "Depending on Python environment used GEOS might need to be installed via "
+            "`brew install geos`."
+        )
+    elif sys.platform.startswith("linux"):
+        platform_specific_msg = (
+            "Depending on Python environment used GEOS might need to be installed via "
+            "system package `libgeos-dev`."
+        )
+    else:
+        platform_specific_msg = "GEOS package will need to be installed see (https://trac.osgeo.org/geos/)"
+    raise ModuleNotFoundError(
+        f"Module 'shapely' not found. Install optionally with `scale-nucleus[metrics]`. {platform_specific_msg}"
+    ) from e
