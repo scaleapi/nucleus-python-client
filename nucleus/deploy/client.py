aa6600e8b553ffe92206c75c70e468715a165cfc
Sean Shi <sean.shi@scale.com>
2022-01-21 11:34:01 -0800

    one more extraneous data

--- nucleus/deploy/client.py
+++ nucleus/deploy/client.py
@@ -175,7 +175,7 @@ class DeployClient:
         elif gpus > 0 and gpu_type is None:
             raise ValueError("If nonzero gpus, must provide gpu_type")
         resp = self.connection.post(payload, ENDPOINT_PATH)
-        endpoint_creation_task_id = resp["data"][
+        endpoint_creation_task_id = resp[
             "endpoint_id"
         ]  # Serverside needs updating
         logger.info(
