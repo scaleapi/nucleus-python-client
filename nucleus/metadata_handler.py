b83f4835ce2ad9750cb3d752c44133d061d98d46
Jean Lucas <jean.ferreira@scale.com>
2022-01-31 18:47:16 -0300

    minor fixes

--- nucleus/metadata_handler.py
+++ nucleus/metadata_handler.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from enum import Enum
 from typing import List, Optional, TYPE_CHECKING, Union
 
@@ -11,17 +12,19 @@ if TYPE_CHECKING:
 
 
 class ExportMetadataType(Enum):
-    SCENES = "scenes"
-    DATASET_ITEMS = "datasetItems"
+    SCENES = "scene"
+    DATASET_ITEMS = "item"
 
 
 def same_keys(dict_a, dict_b):
-    a_keys = sorted(dict_a.keys())
-    b_keys = sorted(dict_b.keys())
-    if (a_keys != b_keys):
-        raise Exception(
-            f"Detected key deletion on metadata. Previous: {dict_b}. New: {dict_a}."
-            f"If this is desired, set overwrite=True when calling the 'update' method."
+    a_keys = set(dict_a.keys())
+    b_keys = set(dict_b.keys())
+    diff = b_keys.difference(a_keys)
+    if (len(diff) > 0):
+        # TODO(Jean): raise Exception here once we support overwrite/replace
+        print(
+            f"Detected key deletion of key(s): {diff}.\n"
+            f"Key deletion is not supported at the moment. Only key addition or updates."
         )
 
 
@@ -29,18 +32,31 @@ class DatasetItemMetadata(DictCompatibleModel):
     reference_id: str
     metadata: dict
 
+    def to_dict(self):
+        return {
+            'reference_id': self.reference_id,
+            'metadata': self.metadata
+        }
+
 
 class SceneMetadata(DictCompatibleModel):
     reference_id: str
     metadata: dict
     dataset_items: List[DatasetItemMetadata]
 
+    def to_dict(self):
+        return {
+            'reference_id': self.reference_id,
+            'metadata': self.metadata,
+            'dataset_items': [item.to_dict() for item in self.dataset_items]
+        }
 
-SceneOrItemMetadata = Union[List[SceneMetadata], List[DatasetItemMetadata]]
+
+SceneOrItemMetadataList = Union[List[SceneMetadata], List[DatasetItemMetadata]]
 ListOrString = Optional[Union[List[str], str]]
 
 
-def validate_scene_items(new_items: SceneOrItemMetadata, old_items_map: dict):
+def validate_scene_items(new_items: SceneOrItemMetadataList, old_items_map: dict):
     # If user deletes a key, and does not set overwrite=True, throw error
     for item in new_items:
         same_keys(item.metadata, old_items_map[item.reference_id])
@@ -66,8 +82,9 @@ class MetadataManager:
         # for internal validation
         self._raw_items_flat_map = {}  # ref_id => metadata
 
-    def _populate_flat_map(self, data: SceneOrItemMetadata):
-        for item in data:
+    def _populate_flat_map(self, data: SceneOrItemMetadataList):
+        data_copy = deepcopy(data)
+        for item in data_copy:
             self._raw_items_flat_map[item.reference_id] = item.metadata
             if isinstance(item, SceneMetadata):
                 for dataset_item in item.dataset_items:
@@ -80,9 +97,9 @@ class MetadataManager:
         resp = self._client.make_request(
             payload=payload,
             requests_command=requests.get,
-            route=f"dataset/{self.dataset_id}/metadata_by_type/{ExportMetadataType.SCENES.value}",
+            route=f"dataset/{self.dataset_id}/metadata_by_level/{ExportMetadataType.SCENES.value}",
         )
-
+        print(resp)
         scene_items = [SceneMetadata.parse_obj(resp_item) for resp_item in resp["scenes_metadata"]]
         self._populate_flat_map(scene_items)
         return scene_items
@@ -94,21 +111,22 @@ class MetadataManager:
         resp = self._client.make_request(
             payload=payload,
             requests_command=requests.get,
-            route=f"dataset/{self.dataset_id}/metadata_by_type/{ExportMetadataType.DATASET_ITEMS.value}",
+            route=f"dataset/{self.dataset_id}/metadata_by_level/{ExportMetadataType.DATASET_ITEMS.value}",
         )
         dataset_items = [DatasetItemMetadata.parse_obj(resp_item) for resp_item in resp["items_metadata"]]
         self._populate_flat_map(dataset_items)
         return dataset_items
 
-    def update(self, items: SceneOrItemMetadata, overwrite: bool = False):
-        if (overwrite):
-            # TODO(Jean): Should we request user confirmation for this? Or put a timeout so they can cancel?
-            print("Overwrite flag set to True, original metadata will be deleted, and replaced by the new one.")
-        else:
-            validate_scene_items(items, self._raw_items_flat_map)
+    def update(self, items: SceneOrItemMetadataList):
+        # TODO(Jean): Support overwrite/replace in the future
+        validate_scene_items(items, self._raw_items_flat_map)
 
-        return
-        payload = {"metadata": items, "overwrite": overwrite, "is_scene": isinstance(items, SceneMetadata)}
+        if isinstance(items, (SceneMetadata, DatasetItemMetadata)):
+            items = [items]
+
+        dict_items = [item.to_dict() for item in items]
+        is_scene = isinstance(items[0], SceneMetadata)
+        payload = {"metadata": dict_items, "is_scene": is_scene}
         resp = self._client.make_request(payload=payload, route=f"dataset/{self.dataset_id}/metadata")
         return resp
 
