87a279a566a0d0358534450f4a90de6610a51bf5
Drake Wong <drakejwong@gmail.com>
2022-02-16 10:03:06 -0800

    black

--- cli/helpers/web_helper.py
+++ cli/helpers/web_helper.py
@@ -6,7 +6,7 @@ from cli.helpers.nucleus_url import nucleus_url
 def launch_web_or_show_help(
     sub_url: str, ctx: click.Context, launch_browser: bool
 ):
-    """ Launches the sub_url (composed with nuclues_url(sub_url)) in the browser if requested"""
+    """Launches the sub_url (composed with nuclues_url(sub_url)) in the browser if requested"""
     if not ctx.invoked_subcommand:
         if launch_browser:
             url = nucleus_url(sub_url)
