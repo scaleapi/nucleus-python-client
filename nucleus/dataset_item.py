3654d4b0393ea71c013b3f9243d4efb22ac1e64e
Claire Pajot <claire.pajot@scale.com>
2022-03-02 18:05:03 +0000

    Switch the dataset item type

--- nucleus/dataset_item.py
+++ nucleus/dataset_item.py
@@ -233,12 +233,13 @@ class DatasetItem:  # pylint: disable=R0902
         )
         self.type = (
             DatasetItemType.IMAGE
-            if self.image_location
-            else (
-                DatasetItemType.POINTCLOUD
-                if self.pointcloud_location
-                else DatasetItemType.VIDEO
-            )
+            if self.image_location or self.video_frame_location
+            else DatasetItemType.POINTCLOUD
+            # else (
+            #     DatasetItemType.POINTCLOUD
+            #     if self.pointcloud_location
+            #     else DatasetItemType.VIDEO
+            # )
         )
         camera_params = (
             self.metadata.get(CAMERA_PARAMS_KEY, None)
