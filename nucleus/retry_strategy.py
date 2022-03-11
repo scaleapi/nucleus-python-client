5fe4e6bbaefd34c2adfa2ee98767cf1082838c12
Ubuntu <diego.ardila@scale.com>
2022-03-11 17:38:22 +0000

    work in progress

--- nucleus/retry_strategy.py
+++ nucleus/retry_strategy.py
@@ -1,4 +1,4 @@
 # TODO: use retry library instead of custom code. Tenacity is one option.
 class RetryStrategy:
     statuses = {503, 524, 520, 504}
-    sleep_times = [1, 3, 9]
+    sleep_times = [1, 3, 9, 27]  # These are in seconds
