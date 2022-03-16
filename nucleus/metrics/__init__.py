e55e7cc0b1e09338f00e14c6599280ccca090eab
Anirudh-Scale <anirudh.jain@scale.com>
2022-03-16 23:59:51 +0000

    3D Cuboid Metrics
    linting for circle ci
    
    version
    
    used native polygon
    
    adding shapely
    
    adding shapely
    
    changing shapely
    
    changing shapely
    
    updating shapely
    
    poetry added shapely
    
    edge case
    
    np type

--- nucleus/metrics/__init__.py
+++ nucleus/metrics/__init__.py
@@ -1,5 +1,6 @@
 from .base import Metric, ScalarResult
 from .categorization_metrics import CategorizationF1
+from .cuboid_metrics import CuboidIOU, CuboidPrecision, CuboidRecall
 from .polygon_metrics import (
     PolygonAveragePrecision,
     PolygonIOU,
