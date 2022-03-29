cf78f96b14e019bf2ad369ca3c98d6f4bfd4c45b
Sasha Harrison <sasha.harrison@scale.com>
2022-03-29 14:59:19 -0700

    fix mypy

--- nucleus/validate/client.py
+++ nucleus/validate/client.py
@@ -83,7 +83,7 @@ class Validate:
                 name=name,
                 slice_id=slice_id,
                 evaluation_functions=[
-                    ef.to_entry() for ef in evaluation_functions
+                    ef.to_entry() for ef in evaluation_functions  # type:ignore
                 ],
             ).dict(),
             "validate/scenario_test",
