a675627eefe73ec150d140c506f4333d7dd3ff0b
Gunnar Atli Thoroddsen <gunnar.thoroddsen@scale.com>
2022-04-05 13:02:18 +0200

    Import shapely in Cuboid utils only and only raise ModuleNotFound on usage

--- nucleus/metrics/__init__.py
+++ nucleus/metrics/__init__.py
@@ -1,5 +1,3 @@
-import sys
-
 from .base import Metric, ScalarResult
 from .categorization_metrics import CategorizationF1
 from .cuboid_metrics import CuboidIOU, CuboidPrecision, CuboidRecall
@@ -17,23 +15,3 @@ from .polygon_metrics import (
     PolygonPrecision,
     PolygonRecall,
 )
-
-try:
-    import shapely
-except ModuleNotFoundError as e:
-    if sys.platform.startswith("darwin"):
-        platform_specific_msg = (
-            "Depending on Python environment used GEOS might need to be installed via "
-            "`brew install geos`."
-        )
-    elif sys.platform.startswith("linux"):
-        platform_specific_msg = (
-            "Depending on Python environment used GEOS might need to be installed via "
-            "system package `libgeos-dev`."
-        )
-    else:
-        platform_specific_msg = "GEOS package will need to be installed see (https://trac.osgeo.org/geos/)"
-    raise ModuleNotFoundError(
-        f"Module 'shapely' not found. Install optionally with `scale-nucleus[shapely]` or when developing "
-        f"`poetry install -E shapely`. {platform_specific_msg}"
-    ) from e
--- nucleus/metrics/cuboid_utils.py
+++ nucleus/metrics/cuboid_utils.py
@@ -2,7 +2,32 @@ from functools import wraps
 from typing import Dict, List, Tuple
 
 import numpy as np
-from shapely.geometry import Polygon
+
+try:
+    from shapely.geometry import Polygon
+except ModuleNotFoundError:
+    import sys
+
+    class Polygon:  # type: ignore
+        def __init__(self, *args, **kwargs):
+            """Object to make sure we only raise errors if actually trying to use shapely"""
+            if sys.platform.startswith("darwin"):
+                platform_specific_msg = (
+                    "Depending on Python environment used GEOS might need to be installed via "
+                    "`brew install geos`."
+                )
+            elif sys.platform.startswith("linux"):
+                platform_specific_msg = (
+                    "Depending on Python environment used GEOS might need to be installed via "
+                    "system package `libgeos-dev`."
+                )
+            else:
+                platform_specific_msg = "GEOS package will need to be installed see (https://trac.osgeo.org/geos/)"
+            raise ModuleNotFoundError(
+                f"Module 'shapely' not found. Install optionally with `scale-nucleus[shapely]` or when developing "
+                f"`poetry install -E shapely`. {platform_specific_msg}"
+            )
+
 
 from nucleus.annotation import CuboidAnnotation
 from nucleus.prediction import CuboidPrediction
