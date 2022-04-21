bd15466ccecbee68d2a8cfb4be141d66ea651548
Gunnar Atli Thoroddsen <gunnar.thoroddsen@scale.com>
2022-04-21 16:21:22 +0200

    Adapt tests

---
+++ tests/metrics/test_geometry.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pytest
+from shapely.geometry import LineString, Polygon
+
+RECTANGLE1 = Polygon(
+    [
+        (0, 0),
+        (100, 0),
+        (100, 100),
+        (0, 100),
+    ],
+)
+
+RECTANGLE2 = Polygon(
+    [
+        (50, 50),
+        (50, 150),
+        (150, 150),
+        (150, 50),
+    ],
+)
+
+POLYGON1 = Polygon(
+    [
+        (0, 0),
+        (100, 0),
+        (100, 100),
+        (0, 100),
+    ],
+)
+
+POLYGON2 = Polygon(
+    [
+        (50, 50),
+        (50, 150),
+        (150, 150),
+        (150, 50),
+    ],
+)
+
+SEGMENT1 = LineString([(0, 0), (0, 50)])
+
+SEGMENT2 = LineString([(-25, 25), (25, 25)])
+
+
+def test_segment_intersection():
+    intersection = SEGMENT1.intersection(SEGMENT2)
+    assert intersection.x == pytest.approx(0)
+    assert intersection.y == pytest.approx(25)
+
+
+def test_rectangle_intersection_area():
+    intersection = RECTANGLE1.intersection(RECTANGLE2).area
+    assert intersection == pytest.approx(2500)
+
+
+def test_convex_polygon_intersection_area():
+    intersection = np.abs(POLYGON1.intersection(POLYGON2).area)
+    assert intersection == pytest.approx(2500)
+
+
+def test_polygon_intersection_area():
+    intersection = POLYGON1.intersection(POLYGON2).area
+    assert intersection == pytest.approx(2500)
+
+
+def test_polygon_area():
+    assert RECTANGLE1.area == 10000
+    assert RECTANGLE2.area == 10000
+    assert POLYGON1.area == 10000
+    assert POLYGON2.area == 10000
