f310f4e5f76e8c09be6c5f86113571adc1edb86f
Sasha Harrison <sasha.harrison@scale.com>
2022-07-12 16:14:57 -0700

    none value for float is ok

--- nucleus/validate/utils.py
+++ nucleus/validate/utils.py
@@ -1,7 +1,9 @@
 from typing import Optional
 
 
-def try_convert_float(float_str: str) -> Optional[float]:
+def try_convert_float(float_str: Optional[str] = None) -> Optional[float]:
+    if float_str is None:
+        return None
     try:
         return float(float_str)
     except ValueError:
