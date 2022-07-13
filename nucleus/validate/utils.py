2d15831f7ce5feb8a6db36f3cc7db5079392ab1d
Sasha Harrison <sasha.harrison@scale.com>
2022-07-13 18:42:04 -0400

    implement a better way by also catching type errors

--- nucleus/validate/utils.py
+++ nucleus/validate/utils.py
@@ -1,10 +1,8 @@
 from typing import Optional
 
 
-def try_convert_float(float_str: Optional[str] = None) -> Optional[float]:
-    if float_str is None:
-        return None
+def try_convert_float(float_str: str) -> Optional[float]:
     try:
         return float(float_str)
-    except ValueError:
+    except (ValueError, TypeError):
         return None
