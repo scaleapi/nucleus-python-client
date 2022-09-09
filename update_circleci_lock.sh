bb282167a78861fd78bd55558364e7e652fb4f54
Gunnar Atli Thoroddsen <gunnar.thoroddsen@scale.com>
2022-09-09 11:23:05 +0200

    Update README.md

--- update_circleci_lock.sh
+++ update_circleci_lock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eux
-docker run --rm -v $PWD:/workspace -w /workspace python:3.6-buster ./create_lockfile.sh
+docker run -it --rm -v $PWD:/workspace -w /workspace python:3.6-buster /bin/bash -c './create_lockfile.sh'
 CIRCLE_CI_LOCK=./poetry.circleci.lock
 cp ./poetry.lock $CIRCLE_CI_LOCK
 
