da61dd63d6ab0b500fbffe807733d6e58a5df66d
Sean Shi <sean.shi@scale.com>
2022-09-23 13:56:13 -0700

    add untested fns for checking/visualizing category + line

--- nucleus/test_launch_integration.py
+++ nucleus/test_launch_integration.py
@@ -8,6 +8,11 @@ from pydantic import BaseModel, Extra, ValidationError
 # These classes specify how user models must pass output to Launch + Nucleus.
 
 
+class PointModel(BaseModel, extra=Extra.forbid):
+    x: float
+    y: float
+
+
 class BoxGeometryModel(BaseModel, extra=Extra.forbid):
     x: float
     y: float
@@ -37,6 +42,19 @@ class CategoryAnnotationModel(BaseModel, extra=Extra.forbid):
     metadata: Optional[Dict[str, Any]] = None
 
 
+class LineGeometryModel(BaseModel, extra=Extra.forbid):
+    vertices: List[PointModel]
+
+
+class LineAnnotationModel(BaseModel, extra=Extra.forbid):
+    geometry: LineGeometryModel
+    type: str
+    label: Optional[str] = None
+    confidence: Optional[float] = None
+    classPdf: Optional[Dict[str, float]] = None
+    metadata: Optional[Dict[str, Any]] = None
+
+
 def verify_output(
     annotation_list: List[Dict[str, Any]],
     model: Type[BaseModel],
@@ -44,8 +62,7 @@ def verify_output(
 ):
     for annotation in annotation_list:
         try:
-            obj = model.parse_obj(annotation)
-            print(obj)
+            model.parse_obj(annotation)
         except ValidationError as e:
             raise ValueError("Failed validation") from e
         if annotation["type"] != annotation_type:
@@ -71,13 +88,40 @@ def verify_category_output(category_list):
     )
 
 
+def verify_line_output(line_list):
+    annotation_type = "line"
+    return verify_output(
+        line_list,
+        LineAnnotationModel,
+        annotation_type,
+    )
+
+
+def _run_model(
+    input_bytes: bytes,
+    load_predict_fn: Callable,
+    load_model_fn: Optional[Callable],
+    model: Optional[Any],
+):
+    if not (model is None) ^ (load_model_fn is None):
+        raise ValueError(
+            "Exactly one of `model` and `load_model_fn` must not be None."
+        )
+
+    if load_model_fn:
+        model = load_model_fn()
+
+    predict_fn = load_predict_fn(model)
+    return predict_fn(input_bytes)
+
+
 def visualize_box_launch_bundle(
     img_file: str,
     load_predict_fn: Callable,
     load_model_fn: Callable = None,
     model: Any = None,
     show_image: bool = False,
-    max_boxes: int = 5,
+    max_annotations: int = 5,
 ) -> Image:
     """
     Run this function locally to visualize what your Launch bundle will do on a local image
@@ -88,28 +132,74 @@ def visualize_box_launch_bundle(
     """
     # Basically do the same thing as what Launch does but locally
 
-    if not (model is None) ^ (load_model_fn is None):
-        raise ValueError(
-            "Exactly one of `model` and `load_model_fn` must not be None."
-        )
-
-    if load_model_fn:
-        model = load_model_fn()
-
-    predict_fn = load_predict_fn(model)
-
     with open(img_file, "rb") as f:
         img_bytes = f.read()
 
-    output = predict_fn(img_bytes)
+    output = _run_model(img_bytes, load_predict_fn, load_model_fn, model)
     verify_box_output(output)
 
     image = Image.open(io.BytesIO(img_bytes))
     draw = ImageDraw.Draw(image)
-    for bbox in output[:max_boxes]:
+    for bbox in output[:max_annotations]:
         geo = bbox["geometry"]
         x, y, w, h = geo["x"], geo["y"], geo["width"], geo["height"]
         draw.rectangle([(x, y), (x + w, y + h)])
+
+    if show_image:
+        image.show()
+
+    return image
+
+
+def run_category_launch_bundle(
+    img_file: str,
+    load_predict_fn: Callable,
+    load_model_fn: Callable = None,
+    model: Any = None,
+):
+    """
+    Not much visualization to be done for category, so we just run it and make sure it works
+    """
+    with open(img_file, "rb") as f:
+        img_bytes = f.read()
+
+    output = _run_model(img_bytes, load_predict_fn, load_model_fn, model)
+    verify_category_output(output)
+    return output
+
+
+def visualize_line_launch_bundle(
+    img_file: str,
+    load_predict_fn: Callable,
+    load_model_fn: Callable = None,
+    model: Any = None,
+    show_image: bool = False,
+    max_annotations: int = 5,
+) -> Image:
+    """
+    Run this function locally to visualize what your Launch bundle will do on a local image
+    Intended to verify that your Launch bundle returns annotations in the correct format, as well as sanity check
+    any coordinate systems used for the image.
+    Will display the image in a separate window if show_image == True.
+    Returns the image
+    """
+    # Basically do the same thing as what Launch does but locally
+    # TODO test
+    # TODO are there even models that return a sequence of lines?
+
+    with open(img_file, "rb") as f:
+        img_bytes = f.read()
+
+    output = _run_model(img_bytes, load_predict_fn, load_model_fn, model)
+    verify_line_output(output)
+
+    image = Image.open(io.BytesIO(img_bytes))
+    draw = ImageDraw.Draw(image)
+    for bbox in output[:max_annotations]:
+        geo = bbox["geometry"]
+        vertices = geo["vertices"]
+        for v1, v2 in zip(vertices[:-1], vertices[1:]):
+            draw.line(v1, v2)
 
     if show_image:
         image.show()
